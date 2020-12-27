3012206e09ca93b0ce3656ce40921fe316a24e3b
Isaac Reinaldo <isaacrpl7@hotmail.com>
2020-12-27 18:32:14 -0300

    Routes created

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 import React from 'react'
-import { FooterContainer } from './containers/footer';
-import { JumbotronContainer } from './containers/jumbotron';
-import { FaqsContainer } from './containers/faqs'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, Browse, Signin, Signup } from './pages';
+import * as ROUTES from './constants/routes';
 
 function App() {
   return (
-    <>
-      <JumbotronContainer/>
-      <FaqsContainer />
-      <FooterContainer/>
-    </>
+    <Router>
+      <Route exact path="/browse">
+        <Browse />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path="/signin">
+        <Signin />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }
 
---
+++ src/pages/signin.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Signip(){
+    return <p>Hello from the sign in!</p>;
+}
