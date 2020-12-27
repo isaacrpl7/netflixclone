7fffb69eff42f5869d8962d19c1450cf5575b84f
Isaac Reinaldo <isaacrpl7@hotmail.com>
2020-12-27 17:54:57 -0300

    Opt-Form added

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -15,7 +15,9 @@ export const Inner = styled.div`
 export const Item = styled.div`
     color: white;
     margin-bottom: 10px;
+    margin: auto;
     max-width: 670px;
+    width: 100%;
 
     &first-of-type {
         margin-top: 3em;
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Accordion } from '../components'
+import OptForm from '../components/opt-form'
 import faqsData from '../fixtures/faqs.json'
 
 export function FaqsContainer() {
@@ -12,9 +13,13 @@ export function FaqsContainer() {
                     <Accordion.Body>{item.body}</Accordion.Body>
                 </Accordion.Item>
             ))}
-            <Accordion.Item>
-
-            </Accordion.Item>
+            <Accordion.Item />
+            <OptForm>
+                <OptForm.Input placeholder="Email address" />
+                <OptForm.Button>Try it now</OptForm.Button>
+                <OptForm.Break />
+                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+            </OptForm>
         </Accordion>
     )
 }
