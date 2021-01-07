b95f9158409258baea6510aaccc477e51faa00f9
Isaac Reinaldo <isaacrpl7@hotmail.com>
2021-01-07 13:32:19 -0300

    Cards and css

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,14 +1,17 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase'
-import { Header, Loading } from '../components';
+import { Card, Header, Loading } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 
 export function BrowseContainer({ slides }) {
+    const [category, setCategory] = useState('series');
     const [searchTerm, setSearchTerm] = useState('');
     const [profile, setProfile] = useState({}); //Cria o estado do perfil a ser escolhido
     const [loading, setLoading] = useState(true); //Cria o estado da tela de carregar
+    const [slideRows, setSlideRows] = useState([]); //as linhas do slide que estão ativas
+
     const { firebase } = useContext(FirebaseContext); // Retorna o contexto do Firebase
     const user = firebase.auth().currentUser || {}; // Retorna as informações do atual usuário usando o contexto que foi retornado
 
@@ -17,6 +20,10 @@ export function BrowseContainer({ slides }) {
             setLoading(false);
         }, 3000);
     }, [profile.displayName]);//Assegura que somente irá rodar o hook novamente, caso o displayName em profile mude
+
+    useEffect(() => {
+        setSlideRows(slides[category]);// Assim que o componente carregar, irá carregar os slides de 'series' ou 'films' dependendo
+    }, [slides, category]);// Caso slides ou categoria mudem, o efeito irá renderizar novamente
 
     return profile.displayName ? ( //Se escolheram um perfil, carregue a página principal, se não, carregue a de escolher perfil
         <>
@@ -30,8 +37,14 @@ export function BrowseContainer({ slides }) {
                 <Header.Frame>
                     <Header.Group>
                         <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-                        <Header.TextLink>Series</Header.TextLink>
-                        <Header.TextLink>Films</Header.TextLink>
+                        <Header.TextLink 
+                            active={category === 'series' ? 'true' : 'false'} 
+                            onClick={()=>setCategory('series')}
+                        >Series</Header.TextLink>
+                        <Header.TextLink 
+                            active={category === 'films' ? 'true' : 'false'} 
+                            onClick={()=>setCategory('films')}
+                        >Films</Header.TextLink>
                     </Header.Group>
                     <Header.Group>
                         <Header.Search
@@ -62,6 +75,28 @@ export function BrowseContainer({ slides }) {
                     <Header.PlayButton>Play</Header.PlayButton>
                 </Header.Feature>
             </Header>
+
+            <Card.Group>
+                {slideRows.map(slideItem => (//para cada item do array que contem objetos de films (ou series, dependendo), retorne:
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                        <Card.Title>{slideItem.title}</Card.Title>
+                        <Card.Entities>
+                            {slideItem.data.map(item => (
+                                <Card.Item key={item.docId} item={item}>
+                                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
+                        </Card.Entities>
+                        <Card.Feature category={category}>
+                            <p>Hello</p>
+                        </Card.Feature>
+                    </Card>
+                ))}
+            </Card.Group>
         </>
     ) : (
         <SelectProfileContainer user={user} setProfile={setProfile} />
