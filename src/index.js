9fcd8ad8116b6376615048d2402df85b1e8ce65c
Isaac Reinaldo <isaacrpl7@hotmail.com>
2021-01-23 11:33:45 -0300

    Bugfix 2 try

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+import App from './App.js';
 import 'normalize.css';
 import { GlobalStyles } from './global-styles';
 import { firebase } from './lib/firebase.prod';
